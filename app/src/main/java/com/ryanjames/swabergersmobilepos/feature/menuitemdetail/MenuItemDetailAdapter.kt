4dbb8d4afc918c2033b993b45fab30d9da3b5e27
Ryan Mallari <ryanjames7994@gmail.com>
2020-05-28 23:18:33 -0700

    Added header

--- app/src/main/java/com/ryanjames/swabergersmobilepos/feature/menuitemdetail/MenuItemDetailAdapter.kt
+++ app/src/main/java/com/ryanjames/swabergersmobilepos/feature/menuitemdetail/MenuItemDetailAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.ryanjames.swabergersmobilepos.R
+import com.ryanjames.swabergersmobilepos.databinding.RowItemProductGroupHeaderBinding
 import com.ryanjames.swabergersmobilepos.databinding.RowItemSelectBinding
 import com.ryanjames.swabergersmobilepos.domain.*
 
@@ -12,6 +13,7 @@ private const val ID_MEAL_OPTION = 1
 private const val ID_PRODUCT_GROUP = 2
 private const val ID_PRODUCT_GROUP_MODIFIER = 3
 private const val ID_QUANTITY = 4
+private const val ID_PRODUCT_GROUP_HEADER = 5
 
 class MenuItemDetailAdapter(
     val product: Product,
@@ -37,6 +39,10 @@ class MenuItemDetailAdapter(
             ID_MEAL_OPTION -> RowSelectMealViewHolder(binding, onClickRowListener)
             ID_PRODUCT_GROUP -> RowSelectProductGroupViewHolder(binding, onClickRowListener)
             ID_QUANTITY -> RowSelectQuantityViewHolder(binding, onClickRowListener)
+            ID_PRODUCT_GROUP_HEADER -> {
+                val headerBinding = RowItemProductGroupHeaderBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                RowProductGroupHeaderViewHolder(headerBinding)
+            }
             else -> RowSelectProductGroupModifierViewHolder(binding, onClickRowListener)
         }
     }
@@ -48,14 +54,19 @@ class MenuItemDetailAdapter(
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         when (holder) {
             is RowSelectMealViewHolder -> {
-                holder.bind(lineItem?.bundle)
+                val dataHolder = data[position] as RowDataHolder.RowSelectMealDataHolder
+                holder.bind(
+                    lineItem?.bundle,
+                    dataHolder.hideLineSeparator
+                )
             }
             is RowSelectProductGroupViewHolder -> {
                 val dataHolder = data[position] as RowDataHolder.RowProductGroupDataHolder
                 val productGroup = dataHolder.productGroup
                 holder.bind(
                     productGroup,
-                    lineItem?.productsInBundle?.get(productGroup) ?: listOf()
+                    lineItem?.productsInBundle?.get(productGroup) ?: listOf(),
+                    dataHolder.hideLineSeparator
                 )
             }
             is RowSelectProductGroupModifierViewHolder -> {
@@ -64,11 +75,17 @@ class MenuItemDetailAdapter(
                 val modifierGroup = dataHolder.modifierGroup
                 holder.bind(
                     product,
-                    modifierGroup, lineItem?.modifiers?.get(ProductModifierGroupKey(product, modifierGroup)) ?: listOf()
+                    modifierGroup, lineItem?.modifiers?.get(ProductModifierGroupKey(product, modifierGroup)) ?: listOf(),
+                    dataHolder.hideLineSeparator
                 )
             }
             is RowSelectQuantityViewHolder -> {
                 holder.bind(lineItem?.quantity ?: 1)
+            }
+            is RowProductGroupHeaderViewHolder -> {
+                val dataHolder = data[position] as RowDataHolder.RowProductGroupHeaderDataHolder
+                val productGroup = dataHolder.productGroup
+                holder.bind(productGroup.productGroupName)
             }
         }
     }
@@ -81,7 +98,7 @@ class MenuItemDetailAdapter(
         private val onClickRowListener: OnClickRowListener
     ) : RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(selectedBundle: ProductBundle?) {
+        fun bind(selectedBundle: ProductBundle?, hideLine: Boolean) {
             binding.tvHeader.setText(R.string.meal_options)
             if (selectedBundle == null) {
                 binding.tvSubheader.setText(R.string.ala_carte)
@@ -89,6 +106,7 @@ class MenuItemDetailAdapter(
                 binding.tvSubheader.text = selectedBundle.bundleName
             }
 
+            binding.view.visibility = if (hideLine) View.GONE else View.VISIBLE
             binding.root.setOnClickListener {
                 onClickRowListener.onClickRowMealOptions()
             }
@@ -101,14 +119,15 @@ class MenuItemDetailAdapter(
         private val onClickRowListener: OnClickRowListener
     ) : RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(productGroup: ProductGroup, productList: List<Product>) {
-            binding.tvHeader.text = "SELECT ${productGroup.productGroupName.toUpperCase()}"
+        fun bind(productGroup: ProductGroup, productList: List<Product>, hideLine: Boolean) {
+            binding.tvHeader.text = "SELECT"
             if (productList.isEmpty()) {
                 binding.tvSubheader.setText(R.string.none_selected)
             } else {
                 binding.tvSubheader.text = productList.toText()
             }
 
+            binding.view.visibility = if (hideLine) View.GONE else View.VISIBLE
             binding.root.setOnClickListener {
                 onClickRowListener.onClickRowProductGroup(productGroup)
             }
@@ -129,7 +148,7 @@ class MenuItemDetailAdapter(
         private val onClickRowListener: OnClickRowListener
     ) : RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(product: Product, modifierGroup: ModifierGroup, modifierList: List<ModifierInfo>) {
+        fun bind(product: Product, modifierGroup: ModifierGroup, modifierList: List<ModifierInfo>, hideLine: Boolean) {
             binding.tvHeader.text = "SELECT ${modifierGroup.modifierGroupName.toUpperCase()}"
             if (modifierList.isEmpty()) {
                 binding.tvSubheader.setText(R.string.none_selected)
@@ -137,6 +156,7 @@ class MenuItemDetailAdapter(
                 binding.tvSubheader.text = "${product.productName} - ${modifierList.toText()}"
             }
 
+            binding.view.visibility = if (hideLine) View.GONE else View.VISIBLE
             binding.root.setOnClickListener {
                 onClickRowListener.onClickRowProductGroupModifier(product, modifierGroup)
             }
@@ -177,6 +197,16 @@ class MenuItemDetailAdapter(
 
     }
 
+    class RowProductGroupHeaderViewHolder(
+        private val binding: RowItemProductGroupHeaderBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(headerText: String) {
+            binding.tvHeader.text = headerText
+        }
+
+    }
+
     private fun createRowDataHolders(): List<RowDataHolder> {
         val list = mutableListOf<RowDataHolder>()
         list.add(RowDataHolder.RowQuantity())
@@ -190,6 +220,13 @@ class MenuItemDetailAdapter(
         }
 
         lineItem?.bundle?.productGroups?.forEach { productGroup ->
+            // Hide line separator for the row before the product group header
+            val lastItemIndex = list.size - 1
+            if (lastItemIndex >= 0) {
+                list[lastItemIndex].hideLineSeparator = true
+            }
+
+            list.add(RowDataHolder.RowProductGroupHeaderDataHolder(productGroup))
             list.add(RowDataHolder.RowProductGroupDataHolder(productGroup))
 
             val productSelection = lineItem?.productsInBundle?.get(productGroup)
@@ -198,8 +235,6 @@ class MenuItemDetailAdapter(
                     list.add(RowDataHolder.RowProductGroupModifierDataHolder(it, modifierGroup))
                 }
             }
-
-
         }
 
         return list
@@ -208,6 +243,7 @@ class MenuItemDetailAdapter(
     private sealed class RowDataHolder {
 
         abstract val itemViewType: Int
+        var hideLineSeparator = false
 
         class RowSelectMealDataHolder : RowDataHolder() {
             override val itemViewType: Int = ID_MEAL_OPTION
@@ -223,6 +259,10 @@ class MenuItemDetailAdapter(
 
         class RowQuantity : RowDataHolder() {
             override val itemViewType: Int = ID_QUANTITY
+        }
+
+        class RowProductGroupHeaderDataHolder(val productGroup: ProductGroup) : RowDataHolder() {
+            override val itemViewType: Int = ID_PRODUCT_GROUP_HEADER
         }
     }
 
